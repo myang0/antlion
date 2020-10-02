c9d3f7383563b0a9f47bfb4d432b9ec022e1b27d
Matthew Yang <39112220+myang0@users.noreply.github.com>
2020-10-01 19:12:41 -0700

    create map for next scene

---
+++ IAT 312 - Antlion Redesign/Assets/Scripts/FightMapManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightMapManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject outerWallPrefab;
+
+    [SerializeField]
+    private GameObject floorTilePrefab;
+
+    public int[, ] grid;
+
+    private int cameraHeight;
+
+    private int rows, columns;
+
+    void Start() {
+        cameraHeight = (int) Camera.main.orthographicSize;
+
+        outerWallPrefab.transform.localScale = new Vector3(1f, 1f, 1f);
+        floorTilePrefab.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        rows = cameraHeight * 2;
+        columns = 11;
+
+        grid = new int[columns, rows];
+
+        for (int colIdx = 0; colIdx < columns; colIdx++) {
+            for (int rowIdx = 0; rowIdx < rows; rowIdx++) {
+                int gridValue = isOuterWall(colIdx, rowIdx) ? 1 : 0;
+
+                grid[colIdx, rowIdx] = gridValue;
+                generateTile(colIdx, rowIdx, gridValue);
+            }
+        }
+    }
+
+    private bool isOuterWall (int column, int row) {
+        return (column == 0 || column == columns - 1 || row == 0 || row == rows - 1);
+    }
+
+    private void generateTile (int column, int row, int value) {
+        Instantiate (floorTilePrefab, new Vector3 (column * 2 - 10, row * 2 - (float) 9, 2), Quaternion.identity);
+
+        if (value == 1) {
+            Instantiate (outerWallPrefab, new Vector3 (column * 2 - 10, row * 2 - (float) 9, 1), Quaternion.identity);
+        }
+    }
+}
--- IAT 312 - Antlion Redesign/Assets/Scripts/InnerWallBehaviour.cs
+++ IAT 312 - Antlion Redesign/Assets/Scripts/InnerWallBehaviour.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InnerWallBehaviour : MonoBehaviour
 {
     [SerializeField]
     private GameObject brokenInnerWallPrefab;
 
+    private bool isQuitting = false;
+
+    void OnApplicationQuit() {
+        isQuitting = true;
+    }
+
     void OnDestroy() {
-        Instantiate(brokenInnerWallPrefab, transform.position, Quaternion.identity);
+        if (!isQuitting) {
+            Instantiate(brokenInnerWallPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
