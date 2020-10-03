847528c813a4fc3c5745c540a19225c0908a1e72
kqrsn <55063627+kqrsn@users.noreply.github.com>
2020-10-02 20:22:35 -0700

    change ant sprite, add ant animation

--- IAT 312 - Antlion Redesign/Assets/Scripts/PlayerMovement.cs
+++ IAT 312 - Antlion Redesign/Assets/Scripts/PlayerMovement.cs
@@ -17,29 +17,37 @@ public class PlayerMovement : MonoBehaviour {
 
     public float kbTimer = 0f;
 
-    void Start() {
+    void Start () {
 
     }
 
-    void Update() {
+    void Update () {
         if (!isStunned) {
-            movement.x = Input.GetAxisRaw("Horizontal");
-            movement.y = Input.GetAxisRaw("Vertical");
+            movement.x = Input.GetAxisRaw ("Horizontal");
+            movement.y = Input.GetAxisRaw ("Vertical");
         }
-       
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Attack();
+
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            Attack ();
         }
 
         if (kbTimer > 0) {
-            rigidBody.AddForce(kbVector * (kbTimer * 0.3f), ForceMode2D.Force);
+            rigidBody.AddForce (kbVector * (kbTimer * 0.3f), ForceMode2D.Force);
             kbTimer--;
             isStunned = (kbTimer != 0);
         }
     }
 
-    void FixedUpdate() {
-        rigidBody.MovePosition(rigidBody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    void FixedUpdate () {
+        rigidBody.MovePosition (rigidBody.position + movement * movementSpeed * Time.fixedDeltaTime);
+
+        if (!isStunned && (movement.x != 0 || movement.y != 0)) {
+            float angle = Mathf.Atan2 (movement.y, movement.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
+        } else if (isStunned) {
+            //for fun
+            // transform.rotation = Quaternion.AngleAxis (rigidBody.rotation + 20, Vector3.forward);
+        }
     }
 
     private void OnTriggerEnter2D (Collider2D collider) {
@@ -73,11 +81,11 @@ public class PlayerMovement : MonoBehaviour {
         Gizmos.DrawWireSphere (attackPoint.position, attackRange);
     }
 
-    void OnCollisionEnter2D(Collision2D col) {
+    void OnCollisionEnter2D (Collision2D col) {
         if (col.gameObject.tag == "Boulder") {
-            Rigidbody2D boulderRb = col.gameObject.GetComponent<Rigidbody2D>();
+            Rigidbody2D boulderRb = col.gameObject.GetComponent<Rigidbody2D> ();
             Vector2 difference = boulderRb.transform.position - transform.position;
-            difference = - difference.normalized;
+            difference = -difference.normalized;
 
             kbVector = difference;
             kbTimer = 240;
