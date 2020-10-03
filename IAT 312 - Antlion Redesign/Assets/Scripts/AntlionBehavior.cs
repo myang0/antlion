9c62ce03515f8b5125c274b26ebfca3ae98ccc71
kqrsn <55063627+kqrsn@users.noreply.github.com>
2020-10-03 16:37:52 -0700

    improve antlion chasing

--- IAT 312 - Antlion Redesign/Assets/Scripts/AntlionBehavior.cs
+++ IAT 312 - Antlion Redesign/Assets/Scripts/AntlionBehavior.cs
@@ -19,42 +19,48 @@ public class AntlionBehavior : MonoBehaviour {
     public Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
-    void Start () {
-        antlion = GameObject.Find ("Antlion");
-        player = GameObject.Find ("Player");
+    void Start() {
+        antlion = GameObject.Find("Antlion");
+        player = GameObject.Find("Player");
     }
 
-    void FixedUpdate () {
+    void FixedUpdate() {
         if (status == Status.Alive) {
             Vector3 playerPos = player.transform.position;
             Vector3 antlionPos = rigidBody.transform.position;
+            if (playerPos.y - antlionPos.y > 2) {
+                playerPos = new Vector3(playerPos.x, playerPos.y + 8, playerPos.z);
+            } else if (playerPos.y - antlionPos.y < -2) {
+                playerPos = new Vector3(playerPos.x, playerPos.y - 8, playerPos.z);
+            }
+
             Vector3 dir = (playerPos - antlionPos).normalized;
 
-            if (Vector3.Distance (playerPos, antlionPos) > 1) {
+            if (Vector3.Distance(playerPos, antlionPos) > 1) {
                 //If player is not behind antlion and antlion isn't too far behind
-                if (playerPos.y - antlionPos.y > 0 && playerPos.y - antlionPos.y < 8) {
-                    rigidBody.MovePosition (antlionPos + dir * movementSpeed * Time.fixedDeltaTime);
+                if (playerPos.y - antlionPos.y > 0 && playerPos.y - antlionPos.y < 16) {
+                    rigidBody.MovePosition(antlionPos +
+                                           dir * (movementSpeed * Time.fixedDeltaTime));
                 } else {
-                    rigidBody.MovePosition (antlionPos + dir * rageMovementSpeed * Time.fixedDeltaTime);
+                    rigidBody.MovePosition(antlionPos +
+                                           dir * (rageMovementSpeed * Time.fixedDeltaTime));
                 }
 
-                float angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
             }
         }
     }
 
-    private void OnCollisionEnter2D (Collision2D collider) {
-        if (collider.gameObject.layer == 8 || collider.gameObject.tag == "Boulder") {
-            Destroy (collider.gameObject);
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.layer == 8 || col.gameObject.CompareTag("Boulder")) {
+            Destroy(col.gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update () {
+    void Update() {
         if ((player.transform.position.y > 12) && status == Status.NotSpawned) {
             spriteRenderer.enabled = true;
-            // boxCollider.enabled = true;
             polyCollider.enabled = true;
             status = Status.Alive;
         }
