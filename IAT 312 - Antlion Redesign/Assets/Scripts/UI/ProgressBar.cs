d401fc9e558847885cb7b42d247610e9e5dd9bd9
Matthew Yang <39112220+myang0@users.noreply.github.com>
2020-10-12 02:33:54 -0700

    add progress bar

---
+++ IAT 312 - Antlion Redesign/Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private GameObject progBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
