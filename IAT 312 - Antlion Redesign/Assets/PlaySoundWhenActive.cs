82b4acf70151a956752b2f37448e8c1f1fd9bec8
kqrsn <55063627+kqrsn@users.noreply.github.com>
2020-10-13 01:03:35 -0700

    win/lose sounds, crossbow sounds

---
+++ IAT 312 - Antlion Redesign/Assets/PlaySoundWhenActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundWhenActive : MonoBehaviour {
+    private AudioSource audio;
+    private bool isPlayed = false;
+    // Start is called before the first frame update
+    void Start() {
+        audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeInHierarchy && !audio.isPlaying && !isPlayed) {
+            audio.Play();
+            isPlayed = true;
+        }
+    }
+}
